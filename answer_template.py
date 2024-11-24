d3dde8ce1f4a235f07e7b3104339be7141c7e938
Sebastian Porras <103103666+SebPorras@users.noreply.github.com>
2024-11-24 15:20:06 +1000

    answer template

---
+++ answer_template.py
@@ -0,0 +1,82 @@
+#answer_template.py
+
+####################### QUESTION 1 #########################
+# List the standard alphabets defined in sym.py 
+# (e.g. Bool_Alphabet). Please ensure your spelling is 
+# correct and your answers are separated by a comma (,).
+############################################################
+
+q1 = " "
+
+####################### QUESTION 2A ########################
+# List the 'special' functions from the Sequence class. Just 
+# enter the function name, for example, for __len__(self) 
+# just enter __len__
+############################################################
+
+q2a = " "
+
+####################### QUESTION 2B ########################
+# In words, describe how the __str__ method is defined for 
+# the Sequence class.
+############################################################
+
+q2b = " " 
+
+####################### QUESTION 3A ########################
+# Submit the first two letters of each identifier type, 
+# separated by a comma.
+############################################################
+
+q3a = " " 
+
+####################### QUESTION 3B ########################
+# Which databases do these identifiers map to? What is the
+# distinction between the different identifier types?
+############################################################
+
+q3b = " " 
+
+
+####################### QUESTION 4A ########################
+# How many proteins with a signal peptide did you find in 
+# Arabidopsis?
+############################################################
+
+q4a = " " 
+
+####################### QUESTION 4B ########################
+# How many proteins involved in lipid metabolism did you 
+# find in Arabidopsis?
+############################################################
+
+q4b = " " 
+
+####################### QUESTION 5 #########################
+# List the ID(s) of candidate TAG lipases in A. thaliana.
+############################################################
+
+q5 = " " 
+
+####################### QUESTION 6A ########################
+# Describe the physio-chemical properties represented by 
+# each default colour used in the alignment.
+############################################################
+
+q6a = " " 
+
+####################### QUESTION 6B ########################
+# List the hydrophobic amino acids which you have 
+# coloured blue.
+############################################################
+
+q6b = " " 
+
+####################### QUESTION 6C ########################
+# Provide the rough boundaries of the fifth transmembrane 
+# domain. Enter your boundaries in the following 
+# format: 10 - 30
+############################################################
+
+q6c = " " 
+
